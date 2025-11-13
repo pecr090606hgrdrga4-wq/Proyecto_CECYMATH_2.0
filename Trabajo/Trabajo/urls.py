7b02055ae54f2f38ea3be7516a1a6d5d4862c99c
Charonrm <charonmr13@gmail.com>
2025-11-13 23:13:35 +0000

    urls_tania

--- Trabajo/Trabajo/urls.py
+++ Trabajo/Trabajo/urls.py
@@ -1,23 +1,18 @@
-"""
-URL configuration for Trabajo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('suma/', include('suma.urls')),
+    path('resta/', include('resta.urls')),
+    path('multiplicacion/', include('multiplicacion.urls')),
+    path('division/', include('division.urls')),
+    path('jerarquia/', include('jerarquia.urls')),
+    path('recta/', include('recta.urls')),
+    path('polinomios/', include('polinomios.urls')),
+    path('ecuaciones1/', include('ecuaciones1.urls')),
+    path('ecuaciones2/', include('ecuaciones2.urls')),
+    path('factorizacion/', include('factorizacion.urls')),
+    path('areas_perimetros/', include('areas_perimetros.urls')),
 ]
